faee0b2602f0b62858d4bb1bbdcb23044f3a97fc
Benjamin Busilana <venaswb@gmail.com>
2017-08-24 17:44:58 -0400

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+
 end
+
+
+def show
+
+end
+
+
+def  new
+  @user.new
+
+end
+
+
+def create
+  @user = User.new(user_params)
+
+end
+
+def edit
+ @user=User.find_by_id(params[:id])
+end
+
+def update
+@user=User.find_by_id(params[:id])
+@user.update(user_params)
+end
+
+private
+
+ def user_params
+    params.require(:user).permit(:name,:email, :id)
+  end
