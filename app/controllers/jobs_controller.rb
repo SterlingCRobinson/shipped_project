1baa2cd5e5a537ab4baaa1950a3e4d55563d78c5
Paul Elion <paul.j.elion@gmail.com>
2017-08-24 17:39:09 -0400

    add jobs controller routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,56 @@
 class JobsController < ApplicationController
+
+	def index
+		@job = Job.all
+	end
+
+
+	def show
+	end
+
+
+	def create
+		@job = Job.new(job_params)
+		if @job.save
+			redirect_to @job, notice: "Job successfully created" 
+		else
+			render :new
+		end
+	end
+
+
+	def new
+		@job = Job.new
+	end
+
+
+	def edit
+	end
+
+
+	def update
+		if @job.update(job_params)
+			redirect_to @job, notice: "Job successfully updated"
+		else
+			render :edit
+	end
+
+
+	def destroy
+		if @job.destroy(job_params)
+			redirect_to @job, notice: "Job successfully deleted"
+		else
+			render :
+		end
+	end
+
+
+	private
+
+	def job_params
+		params.require(:job).permit(:id, :name, :containers, :cost, :description, :origin, :destination)
+	end
+
 end
+
+
