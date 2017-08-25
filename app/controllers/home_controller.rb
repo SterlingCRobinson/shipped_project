dfeee1c8182b144203848571af93fef76759b8a7
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-08-25 16:51:42 -0400

    add boat views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@jobs = Jobs.all.sort_by { |r| r.id }.reverse
-		@job = Jobs.find_by(params[:id])
+		@jobs = Job.all.sort_by { |r| r.id }.reverse
+		@job = Job.find_by(params[:id])
 	end
 end
