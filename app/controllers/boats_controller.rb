7f2c5241824bed88ccd80ed0d98d029c344d3df3
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-08-25 16:05:54 -0400

    added show forms

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,28 +1,35 @@
 class BoatsController < ApplicationController
-	def show
-		@boat = Boat.find_by(params[:id])
+
+	def index
+		@boats = Boat.find_by_id(params[:id])
+	end
+
+	def create
+		@boat = Boat.new(boat_params)
+		if @boat.save
+			redirect_to @boat, notice: "Boat successfully created" 
+		else
+			render :new
+		end
 	end
 
 	def new
 		@boat = Boat.new
 	end
 
-	def create
-		@boat = Boat.new(boat_params)
-		if @boat.save
-			redirect_to boat_path(boat)
-		else
-			render :new
-		end
-	end
-
 	def edit
-		@boat = Boat.find_by(params[:id])
+		@boat = Boat.find_by_id(params[:id])
 	end
 
 	def update
-		@boat = Boat.find_by(params[:id])
-  	@boat.update(boat_params)
+		if @boat.update(boat_params)
+			@boat.update(boat_params)
+		end
+	end
+
+	def destroy
+		@boat.destroy(boat_params)
+		redirect_to @boat, notice: "Boat successfully deleted"
 	end
 
 	private
