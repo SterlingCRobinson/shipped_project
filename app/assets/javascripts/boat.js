2d2bc1975ffcf1f07b9ca3dec1adc2d19a5d0025
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-08-28 12:35:32 -0400

    access to job con added

--- app/assets/javascripts/boat.js
+++ app/assets/javascripts/boat.js
@@ -1,49 +1,48 @@
-// require boat
+function jobsPage(){
+
+	var getJob = document.querySelector("#j_con").innerHTML
+	job = parseInt(getJob, 10)
+	var getBoat = document.querySelector("#b_con").innerHTML
+	boat = parseInt(getBoat, 10)
+	var add = document.querySelector("#add_btn")
+	var ba = document.querySelector("#boatArr")
+	var boatArr = []
 
 
-// function jobsPage(){
+	add.addEventListener("click", function(boat){
+	  boatArr.push(boat)
+	  console.log(boatArr)
+	})
 
-// 	var getJob = document.querySelector("#j_con").innerHTML
-// 	var job = parseInt(getJob, 10)
-// 	var boat = document.querySelector("#b_con")
-// 	var add = document.querySelector("#add_btn")
-// 	var ba = document.querySelector("#boatArr")
+	function delivered(boatArr) {
+	  boatArr.length = 0
+	  console.log(boatArr)
+	}
 
-// 	var boatArr = []
+	function ship(job, boatArr) {
+	  let i = 0
+	  var full = false
+	 boatArr.forEach(function(el) {
+	    while (job > 0) {
+	      if (boatArr[boatArr.length -1] === 10){
+	        full = true
+	        break;
+	      }
+	      if (job === 0){
+	        break;
+	      } else if (boatArr[i] === 10){
+	        i++
+	      } else {
+	      boatArr[i] += 1
+	      jobNow = job -= 1
+	      }
+	    }
+	  })  
+	  console.log(boatArr, jobNow)
+	}
+	getJob.innerHTML = job
+}
 
-
-// 	function addBoat(boat){
-// 	  boatArr.push(boat)
-// 	}
-
-// 	function delivered(boatArr) {
-// 	  boatArr.length = 0
-// 	  console.log(boatArr)
-// 	}
-
-// 	function ship(job, boatArr) {
-// 	  let i = 0
-// 	  var full = false
-// 	 boatArr.forEach(function(el) {
-// 	    while (job > 0) {
-// 	      if (boatArr[boatArr.length -1] === 10){
-// 	        full = true
-// 	        break;
-// 	      }
-// 	      if (job === 0){
-// 	        break;
-// 	      } else if (boatArr[i] === 10){
-// 	        i++
-// 	      } else {
-// 	      boatArr[i] += 1
-// 	      jobNow = job -= 1
-// 	      }
-// 	    }
-// 	  })  
-// 	  console.log(boatArr, jobNow)
-// 	}
-// }
-
-// document.addEventListener("turbolinks:load", function(){
-// 	jobsPage()
-// })
+document.addEventListener("turbolinks:load", function(){
+	jobsPage()
+})
